61a11381a3dd6efcb447409260d95aa5e26492b4
AbdujalilovHasan <abdujalilovhasan171@gmaile.com>
2024-06-05 17:22:09 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,68 @@
+// 1#
+
+// let ism = prompt('Ism kiriting!')
+
+// ism = ism.trim()
+
+// if (ism === "") {
+//     console.log('Ism kiritilmadi.');
+// } else {
+//     let a = ism.charAt(0).toUpperCase() + ism.slice(1).toLowerCase();
+
+//     console.log("Ism: " + a);
+// }
+
+
+
+// 2#
+
+// let text = prompt("Matn kiriting uzunlig 10 bo'lsin.")
+
+// if (text.length > 10) {
+//    console.log(text.slice(10));
+// }
+
+
+
+// 3#
+
+// let num1 = parseInt(prompt("Birinchi sonni kiriting:"));
+// let num2 = parseInt(prompt("Ikkinchi sonni kiriting:"));
+// let num3 = parseInt(prompt("Uchinchi sonni kiriting:"));
+
+// let maxNum = Math.max(num1, num2, num3)
+
+// console.log("Eng katta son: " + maxNum);
+
+
+// 4#
+
+// let ism = prompt('Ism kiriting!')
+// let surName = prompt('Familiyangizni kiriting!')
+// let age = +prompt('Yoshingizni kiriting!')
+
+// if (ism == 'Hasan' && surName == 'Abdujalilov' && age === 14) {
+//     alert('tasdiqlandi')
+// } else {
+//     alert('qayta kiriting!')
+// }
+
+
+
+// 5# 
+
+// let son = +prompt('Son kiriting!')
+
+// if (son / 2) {
+//     alert('Juft son')
+// } else {
+//     alert('toq son')
+// }
+
+
+
+// 6#
+
+/* hello */
+// pokmoi xgdf
+// dfdr 
